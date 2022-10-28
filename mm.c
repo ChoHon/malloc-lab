dc77c2819b2c122f32a9da624b694d15286f6c1c
Chohon <siaksiakx@gmail.com>
2022-10-28 07:52:46 +0000

    함수 순서 변경

--- mm.c
+++ mm.c
@@ -80,91 +80,15 @@ team_t team = {
 // 항상 첫번째 사용가능한 블록을 가리키는 포인터
 static char *free_list_header = NULL;
 
-// 가용 블록 연결
-static void *coalesce(void *bp)
-{
-    // 이전 블록과 다음 블록의 할당 비트
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    
-    // 현재 블록의 size
-    size_t size = GET_SIZE(HDRP(bp));
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
 
-    // 이전 블록과 다음 블록 모두 할당되어 있는 경우
-    if (prev_alloc && next_alloc) return bp;
-
-    // 다음 블록만 가용 블록인 경우
-    else if (prev_alloc && !next_alloc) {
-        // 다음 블록의 크기만큼 size에 더한다
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-
-        // 바뀐 size로 현재 블록의 header와 footer 수정
-        // 다음 블록의 footer을 바꿔야하지 않나? 
-        //     -> 이미 합쳐져서 bp의 footer가 다음 블록의 footer와 동일
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-    }
-
-    // 이전 블록만 가용 블록인 경우
-    else if (!prev_alloc && next_alloc) {
-        // 이전 블록의 크기만큼 size에 다한다
-        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
-
-        // 바뀐 size로 이전 블록의 header와 지금 블록의 footer를 수정
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-
-        // 현재 노드를 이전 노드로 바꿈
-        void *origin_bp = bp;
-        bp = PREV_BLKP(bp);
-
-        PRED(bp) = NULL;
-        SUCC(bp) = SUCC(origin_bp);
-        free_list_header = bp;
-    }
-
-    // 이전 블록과 다음 블록 둘 다 가용 블록인 경우
-    else {
-        // 이전 블록의 크기와 다음 블록의 크기 만큼 size에 더한다
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(FTRP(PREV_BLKP(bp)));
-        
-        // 바뀐 size로 이전 블록의 header와 다음 블록의 footer를 수정
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-
-        // 현재 노드를 이전 노드로 바꿈
-        void *origin_bp = bp;
-        bp = PREV_BLKP(bp);
-
-        // 이전 블록의 predecessor, 다음 블록의 successor 계승
-        PRED(bp) = NULL;
-        SUCC(bp) = SUCC(origin_bp);
-        free_list_header = bp;
-    }
-
-    return bp;
-}
-
-static void *extend_heap(size_t words)
-{
-    char *bp;
-    size_t size;
-
-    // 데이터 정렬을 유지하기 위해 홀수 개수의 words의 블록 크기 조정
-    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-    if ((long)(bp = mem_sbrk(size)) == -1) return NULL;
-
-    // 가용 블록의 header와 footer 초기화
-    PUT(HDRP(bp), PACK(size, 0)); 
-    PUT(FTRP(bp), PACK(size, 0));
-
-    // 가용 블록의 prodecessor와 successor 초기화
-    PRED(bp) = NULL;
-    SUCC(bp) = NULL;
-
-    // 이전 블록이 가용 블록이면 연결
-    return coalesce(bp);
-}
+int mm_init(void);
+void *mm_malloc(size_t size);
+void mm_free(void *bp);
+void *mm_realloc(void *bp, size_t size);
 
 int mm_init(void)
 {
@@ -183,67 +107,6 @@ int mm_init(void)
     // 성공 여부에 따라 return
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL) return -1;
     return 0;
-}
-
-// first fit으로 가용 블록 탐색
-static void *find_fit(size_t asize)
-{
-    // bp를 첫 가용 블록으로 초기화
-    void *bp = free_list_header;
-
-    // size가 0인 블록을 만나기 전까지 = epilogue를 만나기 전까지 = 가용 리스트 끝까지 갈 때까지
-    while (bp != NULL) {
-        // 현재 블록이 할당 받은 블록이거나 블록 size가 필요한 size보다 작으면 다음 블록으로 점프
-        if (GET_SIZE(HDRP(bp)) < asize) bp = SUCC(bp);
-        
-        // 현재 블록이 필요한 size를 담을 수 있는 가용 블록이면 블록 주소 return
-        else return bp;
-    }
-
-    // 현재 heap에 필요한 size를 담을 수 있는 가용 블록이 없으면 NULL return
-    return NULL;
-}
-
-// 가용 블록에서 적당한 크기의 블록 할당
-static void place(void *bp, size_t asize)
-{
-    // 가용 블록 전체 크기
-    size_t csize = GET_SIZE(HDRP(bp));
-
-    // 전체 크기에서 필요한 크기를 빼고 남은 크기
-    size_t remain = csize - asize;
-
-    // 남은 크기가 최소 블록 크기보다 작으면
-    if (remain < (2 * DSIZE)) {
-        // 전체 크기를 모두 할당
-        PUT(HDRP(bp), PACK(csize, 1));
-        PUT(FTRP(bp), PACK(csize, 1));
-
-        // 가용 블록 연결 리스트에서 삭제
-        SUCC(PRED(bp)) = SUCC(bp);
-        PRED(SUCC(bp)) = PRED(bp);
-    }
-
-    // 남은 크기가 최소 블록 크기보다 크거나 같으면
-    else {
-        // 필요한 크기만큼 할당하고
-        PUT(HDRP(bp), PACK(asize, 1));
-        PUT(FTRP(bp), PACK(asize, 1));
-
-        // 가용 블록 연결 리스트에서 삭제
-        SUCC(PRED(bp)) = SUCC(bp);
-        PRED(SUCC(bp)) = PRED(bp);
-
-        // 남은 크기를 가용블록으로 만듬
-        bp = NEXT_BLKP(bp);
-        PUT(HDRP(bp), PACK(remain, 0));
-        PUT(FTRP(bp), PACK(remain, 0));
-
-        // 새로 생긴 가용 블록을 가용 블록 리스트 맨 앞에 연결
-        PRED(bp) = NULL;
-        SUCC(bp) = free_list_header;
-        free_list_header = bp;
-    }
 }
 
 /*  
@@ -328,16 +191,150 @@ void *mm_realloc(void *bp, size_t size)
     return new_bp;
 }
 
+// heap 크기 증가 요청
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
 
+    // 데이터 정렬을 유지하기 위해 홀수 개수의 words의 블록 크기 조정
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1) return NULL;
 
+    // 가용 블록의 header와 footer 초기화
+    PUT(HDRP(bp), PACK(size, 0)); 
+    PUT(FTRP(bp), PACK(size, 0));
 
+    // 가용 블록의 prodecessor와 successor 초기화
+    PRED(bp) = NULL;
+    SUCC(bp) = NULL;
 
+    // 이전 블록이 가용 블록이면 연결
+    return coalesce(bp);
+}
 
+// 가용 블록 연결
+static void *coalesce(void *bp)
+{
+    // 이전 블록과 다음 블록의 할당 비트
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    
+    // 현재 블록의 size
+    size_t size = GET_SIZE(HDRP(bp));
 
+    // 이전 블록과 다음 블록 모두 할당되어 있는 경우
+    if (prev_alloc && next_alloc) return bp;
 
+    // 다음 블록만 가용 블록인 경우
+    else if (prev_alloc && !next_alloc) {
+        // 다음 블록의 크기만큼 size에 더한다
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
 
+        // 바뀐 size로 현재 블록의 header와 footer 수정
+        // 다음 블록의 footer을 바꿔야하지 않나? 
+        //     -> 이미 합쳐져서 bp의 footer가 다음 블록의 footer와 동일
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
 
+    // 이전 블록만 가용 블록인 경우
+    else if (!prev_alloc && next_alloc) {
+        // 이전 블록의 크기만큼 size에 다한다
+        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
 
+        // 바뀐 size로 이전 블록의 header와 지금 블록의 footer를 수정
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
 
+        // 현재 노드를 이전 노드로 바꿈
+        void *origin_bp = bp;
+        bp = PREV_BLKP(bp);
 
+        PRED(bp) = NULL;
+        SUCC(bp) = SUCC(origin_bp);
+        free_list_header = bp;
+    }
 
+    // 이전 블록과 다음 블록 둘 다 가용 블록인 경우
+    else {
+        // 이전 블록의 크기와 다음 블록의 크기 만큼 size에 더한다
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(FTRP(PREV_BLKP(bp)));
+        
+        // 바뀐 size로 이전 블록의 header와 다음 블록의 footer를 수정
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+
+        // 현재 노드를 이전 노드로 바꿈
+        void *origin_bp = bp;
+        bp = PREV_BLKP(bp);
+
+        // 이전 블록의 predecessor, 다음 블록의 successor 계승
+        PRED(bp) = NULL;
+        SUCC(bp) = SUCC(origin_bp);
+        free_list_header = bp;
+    }
+
+    return bp;
+}
+
+// first fit으로 가용 블록 탐색
+static void *find_fit(size_t asize)
+{
+    // bp를 첫 가용 블록으로 초기화
+    void *bp = free_list_header;
+
+    // size가 0인 블록을 만나기 전까지 = epilogue를 만나기 전까지 = 가용 리스트 끝까지 갈 때까지
+    while (bp != NULL) {
+        // 현재 블록이 할당 받은 블록이거나 블록 size가 필요한 size보다 작으면 다음 블록으로 점프
+        if (GET_SIZE(HDRP(bp)) < asize) bp = SUCC(bp);
+        
+        // 현재 블록이 필요한 size를 담을 수 있는 가용 블록이면 블록 주소 return
+        else return bp;
+    }
+
+    // 현재 heap에 필요한 size를 담을 수 있는 가용 블록이 없으면 NULL return
+    return NULL;
+}
+
+// 가용 블록에서 적당한 크기의 블록 할당
+static void place(void *bp, size_t asize)
+{
+    // 가용 블록 전체 크기
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    // 전체 크기에서 필요한 크기를 빼고 남은 크기
+    size_t remain = csize - asize;
+
+    // 남은 크기가 최소 블록 크기보다 작으면
+    if (remain < (2 * DSIZE)) {
+        // 전체 크기를 모두 할당
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+
+        // 가용 블록 연결 리스트에서 삭제
+        SUCC(PRED(bp)) = SUCC(bp);
+        PRED(SUCC(bp)) = PRED(bp);
+    }
+
+    // 남은 크기가 최소 블록 크기보다 크거나 같으면
+    else {
+        // 필요한 크기만큼 할당하고
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+
+        // 가용 블록 연결 리스트에서 삭제
+        SUCC(PRED(bp)) = SUCC(bp);
+        PRED(SUCC(bp)) = PRED(bp);
+
+        // 남은 크기를 가용블록으로 만듬
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(remain, 0));
+        PUT(FTRP(bp), PACK(remain, 0));
+
+        // 새로 생긴 가용 블록을 가용 블록 리스트 맨 앞에 연결
+        PRED(bp) = NULL;
+        SUCC(bp) = free_list_header;
+        free_list_header = bp;
+    }
+}
